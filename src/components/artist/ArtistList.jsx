3fd15d5ae1cccacadc4980dacc09bc629f02d4b3
cmmerritt <christiane.merritt@gmail.com>
2021-08-10 13:33:58 -0700

    add artist component and link to artist detail page

--- src/components/artist/ArtistList.jsx
+++ src/components/artist/ArtistList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import Artist from './Artist';
 
 const ArtistList = ({ results }) => {
 
   const listItems = results.map(artist => {
     return (
       <li key={artist.id}>
-        <h1> {artist.name}</h1>
-        {artist.disambiguation ? <h4>{artist.disambiguation}</h4> : null} 
+        <Artist artist={artist}/>
       </li>
     );
   });
