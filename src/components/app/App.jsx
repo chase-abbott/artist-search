9332aa74401ded4bc9c76290f7d7f287b0373267
cmmerritt <christiane.merritt@gmail.com>
2021-08-10 13:49:47 -0700

    start ArtistReleases component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from '../artist/Home';
 import { Switch, Route } from 'react-router-dom';
+import ArtistReleases from '../detail/ArtistReleases';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={Home}/>
+      <Route exact path="/artist-details/:id" component={ArtistReleases}/>
     </Switch>
   );
 }
