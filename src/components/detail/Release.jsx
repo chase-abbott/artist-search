38d1f9b40014a945e3134f35c52ca1849f32f3d7
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-08-10 18:51:39 -0400

    Returning release list

--- src/components/detail/Release.jsx
+++ src/components/detail/Release.jsx
@@ -5,12 +5,15 @@ import { Link } from 'react-router-dom';
 const Release = ({ coverArt, id, title }) => {
   
   return (
-    <Link to={`/recordings/${id}`}>
-      {coverArt ? <img src={`https://coverartarchive.org/release/${id}/front`} alt={title}/>
-        : <img src="https://placekeanu.com/" alt="The ONE"/>
-      }
-      <img/>
-    </Link>
+    <>
+      <p>{title}</p>
+      <Link to={`/recordings/${id}`}>
+        {coverArt ? <img src={`https://coverartarchive.org/release/${id}/front`} alt={title}/>
+          : <img src="../pic.png" alt="The ONE"/>
+        }
+        <img/>
+      </Link>
+    </>
   );
 };
 
