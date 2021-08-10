ac200fbcb29e95ce2f055e5f5ad2726ff4ac9f2d
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-08-10 19:29:23 -0400

    rendered recordings page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Home from '../artist/Home';
 import { Switch, Route } from 'react-router-dom';
 import ArtistReleases from '../detail/ArtistReleases';
+import RecordingsPage from '../recordings/RecordingsPage';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={Home}/>
       <Route exact path="/artist-details/:id" component={ArtistReleases}/>
-      <Route exact path="/recordings/:id"/>
+      <Route exact path="/recordings/:id" component={RecordingsPage} />
     </Switch>
   );
 }
