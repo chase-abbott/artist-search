152bb19919f70dd4cdc4da916b20b02688e70236
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-08-10 19:29:56 -0400

    extra space

---
+++ src/components/recordings/RecordingsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import RecordingsList from './RecordingsList';
+import useRecording from '../state/useRecording';
+
+const RecordingsPage = () => {
+  const { recordings } = useRecording();
+  return <RecordingsList recordings={recordings} />
+
+};
+
+export default RecordingsPage;
